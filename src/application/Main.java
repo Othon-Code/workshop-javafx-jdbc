eea0f94d6e600a7b3a8e0b25c446cf0de4cf33f0
Othon-Code <othonn.ssousa@gmail.com>
2019-07-29 12:11:46 -0300

    Tela Sobre

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,9 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {//quando se instancia deve-se fornecer o caminho da view=>/gui/MainView.fxml
@@ -19,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Aplicacao JavaFX de modelo");
 			primaryStage.show();
@@ -27,6 +30,13 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	public static Scene getmainScene() {
+		return mainScene;
+	}
+	
+	
 
 	public static void main(String[] args) {
 		launch(args);
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -15,35 +24,46 @@ public class MainViewController implements Initializable {
 	private MenuItem menuItemDepart;
 	@FXML
 	private MenuItem menuItemSobre;
-	
+
 	@FXML
 	public void onMenuItemVendAction() {
 		System.out.println("onMenuItemVendAction");
-		
+
 	}
-	
-	
+
 	@FXML
 	public void onMenuItemDepartAction() {
 		System.out.println("onMenuItemDepartAction");
-		
-	}
-	
-	@FXML
-	public void onMenuItemSobreAction() {
-		System.out.println("onMenuItemSobreAction");
-		
-	}
-	
-	
-	
-	
-	
-	
-	@Override
-	public void initialize(URL uri, ResourceBundle rb) {
-		
-		
+
 	}
 
+	@FXML
+	public void onMenuItemSobreAction() {
+		loadView("/gui/About.fxml");
+
+	}
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+
+	}
+
+	private synchronized void loadView(String nomeAbsoluto) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto));
+			VBox novoVBox = loader.load();
+			
+			Scene mainScene = Main.getmainScene();
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(novoVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
+		}
+
+	}
 }
